bcfb841ba225d7dc3298452c02655c1def867711
malpish0n <akasztelan617@gmail.com>
2025-10-13 20:42:45 +0200

    Initial commit

---
+++ src/main/java/com/malpishon/gamertrials/GamerTrialsApplication.java
@@ -0,0 +1,13 @@
+package com.malpishon.gamertrials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerTrialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerTrialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malpishon/gamertrials/GamerTrialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malpishon.gamertrials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerTrialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

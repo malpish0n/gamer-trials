340d16c0f26a94a66ff194874205f91764cbe837
malpish0n <akasztelan617@gmail.com>
2025-10-13 21:46:07 +0200

    Basic setup

--- src/main/java/com/malpishon/gamertrials/GamerTrialsApplication.java
+++ src/main/java/com/malpishon/gamertrials/GamerTrialsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GamerTrialsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GamerTrialsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GamerTrialsApplication.class, args);
+    }
 
 }

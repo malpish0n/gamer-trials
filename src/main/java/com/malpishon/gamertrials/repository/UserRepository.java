510cf460b889279681d37bb65753644788585a16
malpish0n <akasztelan617@gmail.com>
2025-10-24 11:49:12 +0200

    feat(register): added basic functionality for register page

---
+++ src/main/java/com/malpishon/gamertrials/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.malpishon.gamertrials.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.malpishon.gamertrials.model.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+}
